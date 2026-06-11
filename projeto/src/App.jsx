c2fdeb22dee0b66eb494d037f4e4fff0344b733a
daniellmiranda-ux <daniel.l.miranda@ba.estudante.senai.br>
2026-06-10 21:22:18 -0300

    salvando

--- projeto/src/App.jsx
+++ projeto/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Campeoes from "./pages/campeoes";
 import Artilheiros from "./pages/artilheiros";
@@ -10,9 +11,13 @@ function App() {
     <>
       <Header />
 
-      <Home />
-      <Campeoes />
-      <Artilheiros />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/campeoes" element={<Campeoes />} />
+          <Route path="/artilheiros" element={<Artilheiros />} />
+        </Routes>
+      </main>
 
       <Footer />
     </>
