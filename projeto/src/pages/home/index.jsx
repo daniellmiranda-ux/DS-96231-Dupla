3df76ed8f5c7145fdd0cc2ecbd1a11241e53239a
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-06-15 19:58:54 -0300

    salvando

--- projeto/src/pages/home/index.jsx
+++ projeto/src/pages/home/index.jsx
@@ -3,10 +3,10 @@ import "./style.css";
 export default function Home() {
     return (
         <div className='page-home'>
-            <h1>Bem-Vindo a Experiencia Da Copa Do Mundo</h1>
+            <h1>Clinica Maria Albano</h1>
 
             <p>
-                A Copa do Mundo de Futebol é o principal torneio de seleções nacionais do mundo, organizado pela FIFA. A primeira edição aconteceu em 1930, no Uruguai, que também foi o primeiro campeão. O torneio é realizado a cada quatro anos, com exceção de 1942 e 1946, quando foi cancelado devido à Segunda Guerra Mundial. Ao longo da história, a competição se tornou um dos eventos esportivos mais importantes e assistidos do planeta, revelando grandes seleções e jogadores. O Brasil é o maior vencedor, com cinco títulos mundiais, e nomes como Pelé, Diego Maradona e Lionel Messi marcaram a história do torneio. Além de promover o esporte, a Copa do Mundo une diferentes países e culturas, sendo considerada a competição mais importante do futebol mundial.
+                O Hospital da Clima - Clínica Maria Albano é uma das instituições de saúde mais tradicionais de Candeias, na Região Metropolitana de Salvador. Fundado em 1977, começou como Clínica Maria Albano e, ao longo das décadas, expandiu sua estrutura até se tornar um hospital-dia e centro de atendimento médico de referência para Candeias e cidades vizinhas.
             </p>
         </div>
 
